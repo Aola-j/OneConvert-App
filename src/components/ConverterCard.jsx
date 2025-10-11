14c91280f4466cb07871a20bdede5cbbb9414269
Aola-j <okunolaabe@gmail.com>
2025-10-11 17:42:55 -0500

    Added currency dropdown/arrow/input-field/conver-button functions

--- src/components/ConverterCard.jsx
+++ src/components/ConverterCard.jsx
@@ -1,11 +1,52 @@
-function ConverterCard({ children }) {
+function ConverterCard() {
   return (
-    <div className="gradient-card mt-10 pb-100 md:mx-10 lg:mx-35 shadow-lg">
-      {children}
-      <div className="flex justify-between flex-wrap lg:ml-20 md:ml-20 lg:mr-150 md:mr-75 items-center pt-20">
-        <h3 className="font-semibold text-3xl">From:</h3>
-        <h3 className="font-semibold text-3xl">To:</h3> 
+    <div className="gradient-card mt-10 mx-auto max-w-5xl p-10 rounded-lg shadow-lg">
+      <div className="lg:p-10">
+      <div className="flex justify-between mb-6">
+        <h3 className="font-semibold text-xl md:text-2xl">From:</h3>
+        <h3 className="font-semibold text-xl md:text-2xl mr-49">To:</h3> 
       </div>
+
+      {/* Currency Dropdown */}
+      <div className="flex justify-between mb-6">
+        <select className="gradient-dropdown p-3 w-60 md:w-58">
+            <option>USD</option>
+            <option>EUR</option>
+            <option>GBP</option>
+        </select>
+
+         <select className="gradient-dropdown p-3 rounded border w-60 md:w-58">
+            <option>USD</option>
+            <option>EUR</option>
+            <option>GBP</option>
+        </select>
+      </div>
+
+      {/* Swap Arrow icon */}
+      <div className="flex justify-center my-4 text-3xl font-bold">⇄</div>
+
+      {/* Input fields */}
+      <div className="flex justify-between mb-6">
+        <input
+        type="number"
+        placeholder=""
+        className="gradient-dropdown p-3 rounded border w-60 md:w-58"
+        />
+
+        <input
+        type="number"
+        placeholder=""
+        className="gradient-dropdown p-3 rounded border w-60 md:w-58"
+        />
+      </div>
+
+      {/* Convert button */}
+      <div className="flex justify-center">
+        <button className="gradient-dropdown p-3 rounded border w-40 md:w-48 text-xl">
+            Convert
+        </button>
+      </div>
+    </div>
     </div>
   );
 }
