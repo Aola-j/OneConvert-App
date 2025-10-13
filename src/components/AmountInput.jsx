e42484838fd9b0d8d0d451c1cedd33b4ea88ec0c
Aola-j <okunolaabe@gmail.com>
2025-10-13 09:46:45 -0500

    Added functionality to AmountInput.jsx feature

--- src/components/AmountInput.jsx
+++ src/components/AmountInput.jsx
@@ -1,18 +1,25 @@
-function AmountInput () {
+function AmountInput ({ fromAmount, toAmount, setFromAmount, disabled}) {
     return (
         <div>
             {/* Input fields */}
-      <div className="flex justify-between mb-6">
+      <div className="flex justify-between flex-wrap mb-6">
+        {/* From amount */}
         <input
         type="number"
+        min="0"
+        step="0.01"
+        value={fromAmount}
+        onChange={(e) => setFromAmount(e.target.value)}
         placeholder=""
         className="gradient-dropdown p-3 rounded border w-60 md:w-58"
         />
-
+        {/* To amount */}
         <input
-        type="number"
+        type="text"
+        value={toAmount}
+        disabled={disabled}
         placeholder=""
-        className="gradient-dropdown p-3 rounded border w-60 md:w-58"
+        className="gradient-dropdown p-3 rounded border w-60 md:w-58 text-gray cursor-not-allowed"
         />
       </div>
         </div>
