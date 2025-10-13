c3804b60000fd1d8e57e5d739139228481519ea8
Aola-j <okunolaabe@gmail.com>
2025-10-13 10:29:07 -0500

    Added major components function for intraction

--- src/components/ConverterCard.jsx
+++ src/components/ConverterCard.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
 import AmountInput from "./AmountInput";
 import ConvertButton from "./ConvertButton";
 import CurrencySelector from "./CurrencySelector";
 
 function ConverterCard() {
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [fromAmount, setFromAmount] = useState("")
+  const [toAmount, setToAmount] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  {/* Handling conversion*/}
+  const handleConvert = async () => {
+  if (!fromAmount || isNaN(fromAmount)) {
+    setError("Please enter a valid amount");
+    return;
+  }
+
+  setLoading(true);
+  setError("");
+  
+  try {
+    const res = await fetch(
+      `https://v6.exchangerate-api.com/v6/f82a68a26438ec9c57bc940d/latest/${fromCurrency}`
+    );
+    const data = await res.json();
+
+    if (data.result === "error") {
+      setError("Failed to fetch exchange rates");
+      return;
+    }
+
+    const rate = data.conversion_rates[toCurrency];
+    const convertedValue = (parseFloat(fromAmount) * rate).toFixed(2);
+    setToAmount(convertedValue);
+  } catch (err) {
+    console.error(err);
+    setError("Something went wrong!");
+  } finally {
+    setLoading(false);
+  }
+};
+
+
   return (
     <div className="gradient-card mt-10 mx-auto max-w-5xl p-10 rounded-lg shadow-lg">
       <div className="lg:p-10">
@@ -10,13 +51,30 @@ function ConverterCard() {
         <h3 className="font-semibold text-xl md:text-2xl">From:</h3>
         <h3 className="font-semibold text-xl md:text-2xl mr-49">To:</h3> 
       </div>
-      <CurrencySelector />
+
+
+      <div className="flex justify-between mb-6">
+        <CurrencySelector selectedCurrency={fromCurrency}
+        onChange={setFromCurrency}
+        />
+      <CurrencySelector selectedCurrency={toCurrency}
+      onChange={setToCurrency}
+      />
+      </div>
+      
 
       {/* Swap Arrow icon */}
       <div className="flex justify-center my-4 text-3xl font-bold">⇄</div>
 
-      <AmountInput />
-      <ConvertButton />
+      <AmountInput  fromAmount={fromAmount}
+        toAmount={toAmount}
+        setFromAmount={setFromAmount}
+        disabled />
+      <ConvertButton  onClick={handleConvert} loading={loading}/>
+
+        {error && (
+        <p className="text-red-600 text-center font-medium mt-4">{error}</p>
+      )}
     </div>
     </div>
   );
