93541209ed747a2d5b1cc7da43a9dad37de2545d
Aola-j <okunolaabe@gmail.com>
2025-10-14 15:16:12 -0500

    Added search function/ dynamically load currency from api

--- src/components/CurrencySelector.jsx
+++ src/components/CurrencySelector.jsx
@@ -1,14 +1,56 @@
-import ConverterCard from "./ConverterCard";
+import { useEffect, useState } from "react"
 
-function CurrencySelector () {
+function CurrencySelector ({ selectedCurrency, onChange }) {
+
+const [currencies, setCurrencies] = useState([])
+const [loading, setLoading] = useState(true)
+const [search, setSearch] = useState("")
+
+useEffect(() => {
+    const fetchCurrencies = async () => {
+
+    try{
+        const res = await fetch(
+            "https://v6.exchangerate-api.com/v6/f82a68a26438ec9c57bc940d/latest/USD"
+        )
+        const data = await res.json()
+        setCurrencies(Object.keys(data.conversion_rates))
+    } catch (err) {
+        console.error("Error loading currencies", err)
+    } finally {
+        setLoading(false)
+    }
+ }
+ fetchCurrencies()
+}, [])
+
+const filteredCurrencies = currencies.filter((c) => c.toLowerCase().includes(search.toLowerCase()))
+
+
     return (
         <div>
-            {/* Currency Dropdown */}
+        <input
+        type="text"
+        placeholder="Search currency..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="border p-2 rounded w-full mb-2"
+      />
+
       <div className="flex justify-between mb-6">
-        <select className="gradient-dropdown p-3 w-60 md:w-58">
-            <option>USD</option>
-            <option>EUR</option>
-            <option>GBP</option>
+        <select 
+        value={selectedCurrency}
+        onChange={(e) => onChange(e.target.value)}
+        className="gradient-dropdown p-3 w-60 md:w-58">
+            {loading ? (
+                <option>Loading...</option>
+            ) : (
+                filteredCurrencies.map((currency) => (
+                    <option key={currency} value={currency}>
+                        {currency}
+                    </option>
+                ))
+            )}
         </select>
       </div>
         </div>
