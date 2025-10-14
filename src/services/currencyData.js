a492414f9d25c4f926c1cedc7e1124b42d276376
Aola-j <okunolaabe@gmail.com>
2025-10-14 17:49:32 -0500

    Added currency data object

---
+++ src/services/currencyData.js
@@ -0,0 +1,17 @@
+const currencyData = {
+  USD: { name: "United States Dollar", flag: "🇺🇸" },
+  EUR: { name: "Euro", flag: "🇪🇺" },
+  GBP: { name: "British Pound", flag: "🇬🇧" },
+  NGN: { name: "Nigerian Naira", flag: "🇳🇬" },
+  CAD: { name: "Canadian Dollar", flag: "🇨🇦" },
+  AUD: { name: "Australian Dollar", flag: "🇦🇺" },
+  JPY: { name: "Japanese Yen", flag: "🇯🇵" },
+  CNY: { name: "Chinese Yuan", flag: "🇨🇳" },
+  INR: { name: "Indian Rupee", flag: "🇮🇳" },
+  ZAR: { name: "South African Rand", flag: "🇿🇦" },
+  CHF: { name: "Swiss Franc", flag: "🇨🇭" },
+  SEK: { name: "Swedish Krona", flag: "🇸🇪" },
+  NOK: { name: "Norwegian Krone", flag: "🇳🇴" },
+};
+
+export default currencyData;
