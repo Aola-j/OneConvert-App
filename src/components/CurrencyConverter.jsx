de34d8b0de8e2e1bab29396f610c0ec4e5591fd2
Aola-j <okunolaabe@gmail.com>
2025-10-14 23:12:43 -0500

    Minor styling refix

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -3,7 +3,7 @@ import ConverterCard from "./ConverterCard";
 function CurrencyConverter() {
   return (
     <div className="pt-25 px-4">
-      <h2 className="mt-10 ml-10 font-semibold text-2xl">Currency Converter</h2>
+      <h2 className="mt-10 md:ml-10 lg:ml-10 font-semibold md:text-1xl lg:text-2xl">Currency Converter</h2>
       <ConverterCard />
     </div>
   );
